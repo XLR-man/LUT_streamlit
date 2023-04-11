bbba79bf596e649b1dfba886fdb016597e9b2916
XLR-man <73239630+XLR-man@users.noreply.github.com>
2023-04-11 23:07:58 +0800

    Update runstreamlit.py

--- runstreamlit.py
+++ runstreamlit.py
@@ -14,33 +14,33 @@ st.write(
 st.sidebar.write("## Upload and download :gear:")
 
 
-# Download the fixed image
-def convert_image(img):
-    buf = BytesIO()
-    img.save(buf, format="PNG")
-    byte_im = buf.getvalue()
-    return byte_im
+# # Download the fixed image
+# def convert_image(img):
+#     buf = BytesIO()
+#     img.save(buf, format="PNG")
+#     byte_im = buf.getvalue()
+#     return byte_im
 
 
-def fix_image(upload):
-    image = Image.open(upload)
-    col1.write("Original Image :camera:")
-    col1.image(image)
+# def fix_image(upload):
+#     image = Image.open(upload)
+#     col1.write("Original Image :camera:")
+#     col1.image(image)
 
-    fixed = image_adaptive_lut_evaluation.runforstreamlit(image)
-    col2.write("Ehanced Image :wrench:")
-    col2.image(fixed)
-    st.sidebar.markdown("\n")
-    st.sidebar.download_button("Download ehanced image", convert_image(fixed), "ehanced.png", "image/png")
+#     fixed = image_adaptive_lut_evaluation.runforstreamlit(image)
+#     col2.write("Ehanced Image :wrench:")
+#     col2.image(fixed)
+#     st.sidebar.markdown("\n")
+#     st.sidebar.download_button("Download ehanced image", convert_image(fixed), "ehanced.png", "image/png")
 
 
-col1, col2 = st.columns(2)
-my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+# col1, col2 = st.columns(2)
+# my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
-if my_upload is not None:
-    fix_image(upload=my_upload)
-else:
-    fix_image("./demo_images/sRGB/a1629.jpg")
+# if my_upload is not None:
+#     fix_image(upload=my_upload)
+# else:
+#     fix_image("./demo_images/sRGB/a1629.jpg")
 
 st.write("## Enhance your low light image")
 st.write(
