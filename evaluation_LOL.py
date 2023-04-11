07f0e57a7819c8c28397f4cab7d22fce5399fb34
XLR-man <73239630+XLR-man@users.noreply.github.com>
2023-04-11 22:41:04 +0800

    Update evaluation_LOL.py

--- evaluation_LOL.py
+++ evaluation_LOL.py
@@ -49,14 +49,6 @@ LUT2.eval()
 classifier.load_state_dict(torch.load("saved_models/%s/classifier_%d.pth" % (opt.model_dir, opt.epoch)))
 classifier.eval()
 
-if opt.input_color_space == 'sRGB':
-    dataloader = DataLoader(
-        ImageDataset_sRGB("./data/%s" % opt.dataset_name, mode="test"),
-        batch_size=1,
-        shuffle=False,
-        num_workers=1,
-    )
-
 
 def generator(img):
     pred = classifier(img).squeeze()
@@ -67,57 +59,3 @@ def generator(img):
     _, combine_A = trilinear_(LUT, img)
 
     return combine_A
-
-
-def visualize_result():
-    """Saves a generated sample from the validation set"""
-    out_dir = "images/%s_%d" % (opt.model_dir, opt.epoch)
-    os.makedirs(out_dir, exist_ok=True)
-    for i, batch in enumerate(dataloader):
-        real_A = Variable(batch["A_input"].type(Tensor))
-        img_name = batch["input_name"]
-        fake_B = generator(real_A)
-        save_image(fake_B, os.path.join(out_dir, "%s.png" % (img_name[0][:-4])), nrow=1, normalize=False)
-
-
-def test_speed():
-    t_list = []
-    for i in range(1, 10):
-        img_input = Image.open(os.path.join("./data/fiveK/input/JPG", "original", "a000%d.jpg" % i))
-        img_input = torch.unsqueeze(TF.to_tensor(TF.resize(img_input, (4000, 6000))), 0)
-        real_A = Variable(img_input.type(Tensor))
-        torch.cuda.synchronize()
-        t0 = time.time()
-        for j in range(0, 100):
-            fake_B = generator(real_A)
-
-        torch.cuda.synchronize()
-        t1 = time.time()
-        t_list.append(t1 - t0)
-        print((t1 - t0))
-    print(t_list)
-
-
-def test_image(image_path):
-    """Saves a generated sample from the validation set"""
-    out_dir = "test_images/%s_%d" % (opt.model_dir, opt.epoch)
-    os.makedirs(out_dir, exist_ok=True)
-
-    # Load the image
-    img = Image.open(image_path)
-    img = TF.to_tensor(img)
-    img = img.unsqueeze(0)
-    real_A = Variable(img.type(Tensor))
-    img_name = os.path.split(image_path)[-1]
-    fake_B = generator(real_A)
-    save_image(fake_B, os.path.join(out_dir, "%s" % (img_name)), nrow=1, normalize=False)
-
-
-# ----------
-#  evaluation
-# ----------
-# visualize_result()
-
-# test_speed()
-# image_path = "./demo_images/a0074.jpg"
-# test_image(image_path)
