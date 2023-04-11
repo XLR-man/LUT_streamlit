476ddb8a1e75756171f30bad5fdc2c6ccd1a8116
XLR-man <73239630+XLR-man@users.noreply.github.com>
2023-04-11 22:21:05 +0800

    Update models_x.py

--- models_x.py
+++ models_x.py
@@ -1,3 +1,26 @@
+from setuptools import setup
+import torch
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension, CppExtension
+
+if torch.cuda.is_available():
+    print('Including CUDA code.')
+    setup(
+        name='trilinear',
+        ext_modules=[
+            CUDAExtension('trilinear', [
+                'src/trilinear_cuda.cpp',
+                'src/trilinear_kernel.cu',
+            ])
+        ],
+        cmdclass={
+            'build_ext': BuildExtension
+        })
+else:
+    print('NO CUDA is found. Fall back to CPU.')
+    setup(name='trilinear',
+        ext_modules=[CppExtension('trilinear', ['src/trilinear.cpp'])],
+        cmdclass={'build_ext': BuildExtension})
+    
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
