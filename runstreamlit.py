a019aec54bf5918937289144b4edc47ef3f9bea5
XLR-man <73239630+XLR-man@users.noreply.github.com>
2023-04-12 16:03:19 +0800

    Update runstreamlit.py

--- runstreamlit.py
+++ runstreamlit.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader
 import torch.nn.functional as F
 
 from models_x import *
- st.set_page_config(layout="wide", page_title="Low Light Image Enhancement")
+st.set_page_config(layout="wide", page_title="Low Light Image Enhancement")
 
 st.write("## Enhance your low light dsaimageh121")
 
