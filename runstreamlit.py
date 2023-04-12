33c9a213e52e4cfad5c0060e21477277c00e383f
XLR-man <73239630+XLR-man@users.noreply.github.com>
2023-04-12 14:16:09 +0800

    Update runstreamlit.py

--- runstreamlit.py
+++ runstreamlit.py
@@ -1,3 +1,13 @@
+import subprocess
+import sys
+import torch
+
+if torch.cuda.is_available():
+    print('Including CUDA code.')
+    subprocess.check_call([sys.executable, 'setup.py', 'install'])
+else:
+    print('NO CUDA is found. Fall back to CPU.')
+ 
 import streamlit as st
 import image_adaptive_lut_evaluation
 import evaluation_LOL
