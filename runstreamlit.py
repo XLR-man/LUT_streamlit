71a6145a9c5b30c87608c2cd47a6489b82072316
XLR-man <73239630+XLR-man@users.noreply.github.com>
2023-04-12 15:26:58 +0800

    Update runstreamlit.py

--- runstreamlit.py
+++ runstreamlit.py
@@ -11,7 +11,9 @@ else:
 import streamlit as st
 # import image_adaptive_lut_evaluation
 import time
+st.set_page_config(layout="wide", page_title="Low Light Image Enhancement")
 
+st.write("## Enhance your low light image")
 from torchvision.utils import save_image
 from torch.utils.data import DataLoader
 import torch.nn.functional as F
