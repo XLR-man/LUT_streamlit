99670f0db6b30921760fc9beaad5a4f991dab375
XLR-man <73239630+XLR-man@users.noreply.github.com>
2023-04-12 15:37:00 +0800

    Update runstreamlit.py

--- runstreamlit.py
+++ runstreamlit.py
@@ -30,7 +30,10 @@ else:
     setup(name='trilinear',
         ext_modules=[CppExtension('trilinear', ['src/trilinear.cpp'])],
         cmdclass={'build_ext': BuildExtension})
-    
+
+st.set_page_config(layout="wide", page_title="Low Light Image Enhancement")
+
+st.write("## Enhance your low light dsaimageh")
 import streamlit as st
 # import image_adaptive_lut_evaluation
 import time
